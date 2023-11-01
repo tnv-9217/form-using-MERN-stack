b3f76a56345cf9b26394b74e234871d4c95f5db4
Tanvi Laddha <67188868+tnv-9217@users.noreply.github.com>
2023-11-01 21:53:20 +0530

    Initialize project using Create React App

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const userRouter = require('./routers/user');
+require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.json());
+app.use(userRouter);
+
+app.get('/', (req, res) => {
+ res.send('<h2>This is from index.js file</h2>');
+});
+
+app.listen(PORT, () => {
+ console.log(`server started on port ${PORT}`);
+});
